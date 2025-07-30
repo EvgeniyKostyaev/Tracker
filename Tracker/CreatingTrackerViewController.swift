907a5010cf478fac8ee3ed6f3cb91304cf9c519f
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-07-30 18:55:44 +0400

    [sprint_14] - Created "CreatingTrackerViewController" viewController

---
+++ Tracker/CreatingTrackerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreatingTrackerViewController.swift
+//  Tracker
+//
+//  Created by Evgeniy Kostyaev on 27.05.2025.
+//
+
+import UIKit
+
+final class CreatingTrackerViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Overrides Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Private Methods
+    
+}
