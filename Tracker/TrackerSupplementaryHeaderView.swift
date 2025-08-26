b06d02776203df2f1cd00b66abba00b552403d08
Evgeniy <evgeniy.kostyaev@infobric.com>
2025-08-26 17:24:03 +0400

    [sprint_15] - Added several improvements for "TrackerSupplementaryHeaderView"

--- Tracker/TrackerSupplementaryHeaderView.swift
+++ Tracker/TrackerSupplementaryHeaderView.swift
@@ -18,24 +18,33 @@ final class TrackerSupplementaryHeaderView: UICollectionReusableView {
     // MARK: - Public Properties
     static let identifier = "TrackerSupplementaryHeaderView"
     
-    let titleLabel = UILabel()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: TrackerSupplementaryHeaderViewTheme.fontSize)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     // MARK: - Overrides Methods
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(titleLabel)
-        
-        titleLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:TrackerSupplementaryHeaderViewTheme.leadingConstraint).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: TrackerSupplementaryHeaderViewTheme.trailingConstraint).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        
-        titleLabel.font = UIFont.boldSystemFont(ofSize: TrackerSupplementaryHeaderViewTheme.fontSize)
+        setupLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupLayout() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:TrackerSupplementaryHeaderViewTheme.leadingConstraint),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: TrackerSupplementaryHeaderViewTheme.trailingConstraint),
+            titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
 }
