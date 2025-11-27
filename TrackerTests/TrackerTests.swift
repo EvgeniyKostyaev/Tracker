6a2813251befc1d021c113a0f56b6c25194ee734
Evgeniy Kostyaev <evgeniy.kostyaev@infobric.com>
2025-11-27 15:52:16 +0400

    [sprint_17] - Added "TrackerTests" Unit test target and "swift-snapshot-testing" library

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Evgeniy Kostyaev on 27.11.2025.
+//
+
+import Testing
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
