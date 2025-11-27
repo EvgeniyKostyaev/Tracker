de0f2bbd5e3cef8e52af4b87d9d9c14f18a897e6
Evgeniy Kostyaev <evgeniy.kostyaev@infobric.com>
2025-11-27 16:55:58 +0400

    [sprint_17] - Made snapshot test

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -5,12 +5,25 @@
 //  Created by Evgeniy Kostyaev on 27.11.2025.
 //
 
-import Testing
+import XCTest
+import SnapshotTesting
+@testable import Tracker
 
-struct TrackerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class TrackerTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+    }
+
+    func testViewController() throws {
+        let trackersViewController = TrackersViewController()
+        
+        assertSnapshots(of: {
+            trackersViewController
+        }(), as: [.image])
+    }
 }
